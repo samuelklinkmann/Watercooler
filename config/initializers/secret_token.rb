240c96d0be27ec3c8f508d9fe96fc01d316d9043
Joakim Runeberg <joakim.runeberg@gmail.com>
2011-02-09 10:55:47 +0200

    first commit
    Signed-off-by: Joakim Runeberg <joakim.runeberg@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Watercooler::Application.config.secret_token = '6fcad162dc6cc743204928fe693f25674917e7b378f7e38c8dfa027abdf7e1b5129198015f3d7845f660784819ef25bc077dfbabf8da5babda3bf85a679866e6'
